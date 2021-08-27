497d5fd040216baf9f351863cc2032263181c623
nugaon <toth.viktor.levente@gmail.com>
2021-08-27 17:57:25 +0200

    fix(test): update bee-js and create postage stamp on beedebug

--- jest.config.ts
+++ jest.config.ts
@@ -3,15 +3,15 @@
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/en/configuration.html
  */
-import { Bee } from '@ethersphere/bee-js'
+import { BeeDebug } from '@ethersphere/bee-js'
 import type { Config } from '@jest/types'
 
 export default async (): Promise<Config.InitialOptions> => {
   if (!process.env.BEE_POSTAGE) {
     try {
       console.log('Creating postage stamps...')
-      const beeUrl = process.env.BEE_API_URL || 'http://localhost:1633'
-      const bee = new Bee(beeUrl)
+      const beeDebugUrl = process.env.BEE_DEBUG_API_URL || 'http://localhost:1635'
+      const bee = new BeeDebug(beeDebugUrl)
       process.env.BEE_POSTAGE = await bee.createPostageBatch('1', 20)
       console.log('Queen stamp: ', process.env.BEE_POSTAGE)
       // sleep for 11 seconds (10 blocks with ganache block time = 1s)
